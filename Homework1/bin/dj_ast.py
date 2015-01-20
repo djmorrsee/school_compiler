6676464b4dffaf611d6c658b802d70df530488a9
Daniel Morrissey <djmorrsee@gmail.com>
2015-01-19 19:57:18 -0700

    First flattening steps

--- Homework1/bin/dj_ast.py
+++ Homework1/bin/dj_ast.py
@@ -1,11 +1,53 @@
 import compiler
+import copy
+from compiler.ast import *
+
 import os.path
+
+def flatten_node(n):
+	if isinstance(n, Module):
+		n.node = flatten_node(n.node)
+		return n
+	elif isinstance(n, Stmt):
+		for i in range(0, len(n.nodes)):
+			n.nodes[i] = flatten_node(n.nodes[i])
+		return n
+	elif isinstance(n, Printnl):
+		n.nodes[0] = flatten_node(n.nodes[0])
+		return n
+	elif isinstance(n, Assign):
+		n.expr = flatten_node(n.expr)
+		return n
+	elif isinstance (n, AssName):
+		return n
+	elif isinstance(n, Discard):
+		n.expr = flatten_node(n.expr)
+		return n
+	elif isinstance(n, Const):
+		return n
+	elif isinstance(n, Name):
+		return n
+	elif isinstance(n, Add):
+		n.left = flatten_node(n.left)
+		n.right = flatten_node(n.right)
+		return Const(n.left.value + n.right.value) # as a flattened left and right node should be numerical Const
+	elif isinstance(n, UnarySub):
+		n.expr = flatten_node(n.expr)
+		return n # Const(-n.expr.value)
+	elif isinstance(n, CallFunc):
+		n.node = flatten_node(n.node)
+		return n
+	else:
+		raise Exception('Unknown AST Node' + n)
+
 class dj_ast():
+
+	# Call Flatten on Children, Process, then return self/new tree
+			
+			
 	def __init__(self, src_file):
 		self.filename = os.path.basename(src_file)
-		f = open(src_file)
-		self.raw_tree = compiler.parse("")
-		f.close()
+		self.raw_tree = compiler.parseFile(src_file)
+		self.flattened_tree = flatten_node(copy.deepcopy(self.raw_tree))		
 
-
-
+	
