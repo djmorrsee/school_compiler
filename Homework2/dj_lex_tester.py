df4940eb27a46e903e46ac1d64d7cf306fea28b7
Daniel Morrissey <djmorrsee@gmail.com>
2015-02-04 09:55:29 -0700

    HW2 interview prep

--- Homework2/dj_lex_tester.py
+++ Homework2/dj_lex_tester.py
@@ -4,10 +4,12 @@ import dj_lex
 def main():
 	lexer = dj_lex.lexer
 	for i in range(1, len(sys.argv)):
+		print sys.argv[i]
 		text = open(sys.argv[1])
 		lexer.input(text.read())
 		for j in lexer:
 			print j
+		print
 		
 if __name__ == '__main__':
 	main()
